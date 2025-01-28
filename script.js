109aea6cff3cda02bcf9534374e11efd81f6ddf9
BhaveshYadav001 <bhaveshydv540@gmail.com>
2025-01-28 14:26:32 +0530

    new Game

---
+++ script.js
@@ -0,0 +1,105 @@
+// Mode changing btn
+let newBtn = document.querySelector(".newGame");
+let winnDis = document.querySelector(".winnerDis");
+let modebtn = document.querySelector("#mode");
+let body = document.querySelector("body");
+    let count = 0;
+    let curr = "dark";
+modebtn.addEventListener("click", () => {
+    if (curr === "dark") {
+        curr = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+    else {
+        curr = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }
+    console.log(`${curr} theme`);
+});
+
+// Game Mechanism
+
+
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset");
+
+let turnX = true;
+const winningProb = [
+    [0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 5, 8], [2, 4, 6], [3, 4, 5], [6, 7, 8]
+];
+
+
+const disableBtns = () =>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+const enableBtns = () =>{
+    for(let box of boxes){
+        winnDis.classList.add("hide");
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+const resetBtns = () =>{
+    turnX= true;
+    enableBtns();
+    count = 0;
+};
+
+newBtn.addEventListener("click",resetBtns);
+resetBtn.addEventListener("click",resetBtns);
+
+const counter = () =>{
+    winnDis.innerText = "Match has been Draw";
+    winnDis.classList.remove("hide");
+    console.log("count successfully");
+};
+
+
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turnX === true) {
+            box.classList.remove("box2"); 
+            box.innerText = "X";
+            turnX = false;
+        }
+        else {
+            box.innerText = "O";
+            box.classList.add("box2"); //automatically remove in 'if' condition
+            turnX = true;               
+        }
+        box.disabled = true;
+        count++;
+        console.log(`count ${count}`);
+
+        let winner = checkWinner();
+
+        if(count >= 9 && !winner){
+            counter();
+        }
+    });
+    
+});
+
+const msgDisplay = (winner) =>{
+        winnDis.innerText = `Congratulations Player ${winner} won`;
+        winnDis.classList.remove("hide");
+        disableBtns();
+};
+
+const checkWinner = () => {
+    for (let pattern of winningProb) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+   if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+        if (pos1Val === pos2Val && pos2Val === pos3Val && pos3Val === pos1Val) {
+        msgDisplay(pos1Val);
+        }
+    }
+}
+};
